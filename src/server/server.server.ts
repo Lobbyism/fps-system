17f10f24fd41851a5e527dc8dbbd7cbe8456fc8a
Chad <therobloxchad@gmail.com>
2025-06-19 09:37:20 -0400

    feat: Add notion of shared systems

--- src/server/server.server.ts
+++ src/server/server.server.ts
@@ -1,4 +1,5 @@
 import { ServerScriptService } from "@rbxts/services";
 import { start } from "shared/start";
 
+// Eventually filter systems to consider with game.PlaceId
 start([ServerScriptService.TS.systems], {})();
