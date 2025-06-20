88b48e1a71714cb7434e3ba750f3842a8b4056ea
Chad <therobloxchad@gmail.com>
2025-06-20 13:44:14 -0400

    feat: Add client systems

--- src/client/systems/playerAdded.ts
+++
@@ -1,8 +0,0 @@
-import { System, World } from "@rbxts/matter";
-import { Player } from "shared/components";
-const system: System<[World]> = (world: World) => {
-	for (const [id, playerRecord] of world.queryChanged(Player)) {
-		print(id, playerRecord);
-	}
-};
-export = system;
