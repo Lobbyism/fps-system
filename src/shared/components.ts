5aa02f8662beb01da5821dd176592b8271960b5e
Chad <therobloxchad@gmail.com>
2025-06-24 09:26:28 -0400

    feat: Add components

--- src/shared/components.ts
+++ src/shared/components.ts
@@ -2,9 +2,17 @@ import { AnyEntity, Entity } from "@rbxts/matter";
 import { GenericOfComponent, newComponent } from "@rbxts/matter/lib/component";
 export const Model = newComponent<{ model: Model }>("Model");
 export const Player = newComponent<{ player: Player }>("Player");
-export const Weapon = newComponent<{}>("Weapon");
+export const Weapon = newComponent<{
+	name: string;
+	isShooting: boolean;
+}>("Weapon");
+export const Aim = newComponent<{
+	origin: Vector3;
+	direction: Vector3;
+}>("Aim");
 export const HasWeapon = newComponent<{ serverId: AnyEntity }>("HasWeapon");
-export const components = { Model, Player, Weapon, HasWeapon };
+export const ViewModel = newComponent<{ model: Model }>("ViewModel");
+export const components = { Model, Player, Weapon, HasWeapon, Aim, ViewModel };
 export type ComponentNames = keyof typeof components;
 export type ComponentsMap = { [K in ComponentNames]: MappedComponentToName<K> };
 export type MappedComponentToName<T extends ComponentNames> = GenericOfComponent<ReturnType<(typeof components)[T]>>;
