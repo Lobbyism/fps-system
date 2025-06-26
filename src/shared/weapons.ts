513bcec9e86bbdb1c0f699a4271e0a0e225323a2
Chad <therobloxchad@gmail.com>
2025-06-26 10:21:23 -0400

    feat: Add sliding animation per weapon

--- src/shared/weapons.ts
+++ src/shared/weapons.ts
@@ -5,6 +5,9 @@ export const WeaponUsageState = {
 	Reloading: "Reloading",
 } as const;
 export type WeaponUsageState = (typeof WeaponUsageState)[keyof typeof WeaponUsageState];
+export const MovementState = {
+	Sliding: "Sliding",
+} as const;
 export const WeaponAnimationNames = {
 	fire: "Fire",
 } as const;
@@ -12,6 +15,7 @@ export type WeaponAnimationName = keyof typeof WeaponAnimationNames;
 export type WeaponInfo = {
 	animationIds: {
 		[WeaponUsageState.Shooting]: string;
+		[MovementState.Sliding]: string;
 	};
 	damage: number;
 	fireRate: number;
@@ -27,6 +31,7 @@ export const Weapons = new Map<string, WeaponInfo>([
 		{
 			animationIds: {
 				[WeaponUsageState.Shooting]: "119598271412864",
+				[MovementState.Sliding]: "79994438112815",
 			},
 			damage: 10,
 			fireRate: 9,
