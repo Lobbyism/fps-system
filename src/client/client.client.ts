edf1a55bcea7c97914c80f29e3dbfabe15f435c0
Chad <therobloxchad@gmail.com>
2025-06-30 11:48:03 -0400

    feat: Select camera mode based on PreferredInput

--- src/client/client.client.ts
+++ src/client/client.client.ts
@@ -9,12 +9,24 @@ if (RunService.IsStudio()) {
 	// StarterPlayer.StarterPlayerScripts.TS.systems.cameraModes.thirdPerson.Destroy();
 	cameraMode = Enum.CameraMode.Classic;
 	StarterPlayer.StarterPlayerScripts.TS.systems.cameraModes.firstPerson.Destroy();
+} else {
+	if (UserInputService.PreferredInput === Enum.PreferredInput.KeyboardAndMouse) {
+		cameraMode = Enum.CameraMode.LockFirstPerson;
+		StarterPlayer.StarterPlayerScripts.TS.systems.cameraModes.thirdPerson.Destroy();
+	} else if (UserInputService.PreferredInput === Enum.PreferredInput.Touch) {
+		cameraMode = Enum.CameraMode.Classic;
+		// Technically, both first person and third person camera systems should be active on mobile since the player can toggle between them...
+		StarterPlayer.StarterPlayerScripts.TS.systems.cameraModes.firstPerson.Destroy();
+	}
 }
+assert(cameraMode);
 export interface ClientState {
+	cameraMode: Enum.CameraMode;
 	entityIdMap: Map<string, AnyEntity>;
 	reverseEntityIdMap: Map<AnyEntity, string>;
 }
 start([StarterPlayer.StarterPlayerScripts.TS.systems], {
+	cameraMode: cameraMode,
 	entityIdMap: new Map<string, AnyEntity>(),
 	reverseEntityIdMap: new Map<AnyEntity, string>(),
 })(receiveReplication);
