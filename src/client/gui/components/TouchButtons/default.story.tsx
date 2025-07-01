612d356d1278f9a8223d98fbadde9cf613f8172a
Chad <therobloxchad@gmail.com>
2025-07-01 10:44:27 -0400

    feat: Add sliding and slide cancelling through jumping

--- src/client/gui/components/TouchButtons/default.story.tsx
+++ src/client/gui/components/TouchButtons/default.story.tsx
@@ -3,7 +3,12 @@ import ReactRoblox from "@rbxts/react-roblox";
 import { TouchButtons } from ".";
 export = function (target: Instance) {
 	const handle = ReactRoblox.createRoot(new Instance("Folder"));
-	handle.render(ReactRoblox.createPortal(<TouchButtons setTouchPressedWeaponUsageState={() => {}} />, target));
+	handle.render(
+		ReactRoblox.createPortal(
+			<TouchButtons setTouchPressedWeaponUsageState={() => {}} setTouchPressedMovementState={() => {}} />,
+			target,
+		),
+	);
 	return () => {
 		handle.unmount();
 	};
