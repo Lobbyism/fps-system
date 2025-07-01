999083bb632b3a2c26a7b091f5cdab2b64e9174a
Chad <therobloxchad@gmail.com>
2025-07-01 09:33:31 -0400

    fix: Update other interfaces display

--- src/client/hooks/useApplication/elements/crosshair.tsx
+++ src/client/hooks/useApplication/elements/crosshair.tsx
@@ -4,20 +4,20 @@ import { UIRenderer, UIUpdater } from "..";
 import { World } from "@rbxts/matter";
 import { HasWeapon, Player } from "shared";
 import { Players } from "@rbxts/services";
+let playerHasWeapon = false;
 export const crosshairUpdater: UIUpdater = (world: World) => {
 	let stateChanged = false;
 	for (const [id, hasWeaponRecord] of world.queryChanged(HasWeapon)) {
 		if (!world.contains(id)) continue;
 		const player = world.get(id, Player);
 		if (!player || player.player !== Players.LocalPlayer) continue;
-		if (hasWeaponRecord.new && !hasWeaponRecord.old) {
-			stateChanged = true;
-		} else if (hasWeaponRecord.old && !hasWeaponRecord.new) {
+		if (playerHasWeapon !== (hasWeaponRecord.new !== undefined)) {
 			stateChanged = true;
 		}
+		playerHasWeapon = hasWeaponRecord.new !== undefined;
 	}
 	return stateChanged;
 };
 export const crosshairRenderer: UIRenderer = () => {
-	return <Crosshair />;
+	return playerHasWeapon ? <Crosshair /> : undefined;
 };
